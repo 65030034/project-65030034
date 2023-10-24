a1449503706ac83cc1d3f01bf147b19ab3652d40
65030034 <144875017+65030034@users.noreply.github.com>
2023-10-24 19:42:25 +0700

    Add project files.

---
+++ Project.cs
@@ -0,0 +1,19 @@
+﻿using project_65030034;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_65030034
+{
+    public class Project
+    {
+        public enum Members
+        {
+            Silla = 1,
+            Moke = 2,
+            Saitong =3
+        }
+    }
+}
